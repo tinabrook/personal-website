0d566d5ead5a81964f805d09f5fc8a7a3baa6c46
tinabrook <christina.brookgreen@gmail.com>
2022-09-09 22:59:11 +0800

    Upload website content

--- js/main.js
+++ js/main.js
@@ -55,6 +55,19 @@
         navigationText: false
     });
 
+	/*---------------------------------------------------- */
+	/* Typed Initiation
+	------------------------------------------------------ */ 
+		if ($('.typed-text-output').length == 1) {
+			var typed_strings = $('.typed-text').text();
+			var typed = new Typed('.typed-text-output', {
+				strings: typed_strings.split(', '),
+				typeSpeed: 70,
+				backSpeed: 20,
+				smartBackspace: false,
+				loop: true
+			});
+		}
 
 	/*----------------------------------------------------- */
 	/* Alert Boxes
