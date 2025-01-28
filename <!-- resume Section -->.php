a11f1595c770bab2497b1da5b351e813611130ef
tinabrook <christina.brookgreen@gmail.com>
2025-01-28 21:37:29 +1100

    updating website content

---
+++ <!-- resume Section -->.php
@@ -0,0 +1,199 @@
+   <!-- resume Section -->
+   ================================================== -->
+	<!-- <section id="resume" class="grey-section">
+
+		<div class="row section-intro">
+   		<div class="col-twelve">
+
+   			<h5>Resume</h5>
+   			<h1>More of my credentials.</h1>
+
+   			<p class="lead">Multiple years of professional experience employing quantitative analyses to make evidence-based decisions. I particularly thrive in the "Data Translator" role extracting key insights and conveying them to diverse stakeholders.</p>
+
+   		</div>   		
+   	</div> <!-- /section-intro--> 
+
+   	<div class="row resume-timeline">
+
+   		<div class="col-twelve resume-header">
+
+   			<h2>Work Experience</h2>
+
+   		</div> <!-- /resume-header -->
+
+   		<div class="col-twelve">
+
+   			<div class="timeline-wrap">
+
+   				<div class="timeline-block">
+
+	   				<div class="timeline-ico">
+	   					<i class="fa fa-briefcase"></i>
+	   				</div>
+
+	   				<div class="timeline-header">
+	   					<h3>Manager - Senior Data Scientist</h3>
+	   					<p>2022</p>
+	   				</div>
+
+	   				<div class="timeline-content">
+	   					<h4>Nous Group Consulting</h4>
+	   					<p><a href="nousgroup.com">Nous</a> is the largest Australian-owned consulting firm, focused on delivering positive impact to communities. Managed small-medium scale projects across health, human services and education. Projects included data strategy development, quantiative analysis, data management and interpretation facilitation. Skills: R, SQL, Power BI, Data Analytics, Project Management.</p>
+	   				</div>
+
+	   			</div> <!-- /timeline-block -->
+
+	   			<div class="timeline-block">
+
+	   				<div class="timeline-ico">
+	   					<i class="fa fa-briefcase"></i>
+	   				</div>
+
+	   				<div class="timeline-header">
+	   					<h3>Manager, Senior Consultant, Consultant - Public Policy</h3>
+	   					<p>2019 - 2022</p>
+	   				</div>
+
+	   				<div class="timeline-content">
+	   					<h4>Nous Group Consulting</h4>
+	   					<p><a href="nousgroup.com">Nous</a> is the largest Australian-owned consulting firm, focused on delivering positive impact to communities. Managed medium-large scale projects across health, human services and education. Projects included evaluations, strategy developlemt, and workshop facilitations.</p>
+	   				</div>
+
+	   			</div> <!-- /timeline-block -->
+
+	   			<div class="timeline-block">
+
+	   				<div class="timeline-ico">
+	   					<i class="fa fa-briefcase"></i>
+	   				</div>
+
+	   				<div class="timeline-header">
+	   					<h3>Business Analyst</h3>
+	   					<p>2018 - 2019</p>
+	   				</div>
+
+	   				<div class="timeline-content">
+	   					<h4>Middlebury Digital Learning Inquiry</h4>
+	   					<p><a href="https://dlinq.middcreate.net/">Middlebury's Office of Digital Learning and Inquiry (DLINQ)</a> was launched in Jan 2018 with plans to expand Middlebury curriculums to a digital platform, offering graduate certificates to more students.
+							Conducted market analysis to inform the business strategy for the expansion of higher education curricula to a digital platform. </p>
+	   				</div>
+
+	   			</div> <!-- /timeline-block -->
+
+				   <div class="timeline-block">
+
+					<div class="timeline-ico">
+						<i class="fa fa-briefcase"></i>
+					</div>
+
+					<div class="timeline-header">
+						<h3>Founder & Director</h3>
+						<p>2016 - 2018</p>
+					</div>
+
+					<div class="timeline-content">
+						<h4>Maui & the Movement</h4>
+						<p>Founded and directed a non-profit organisation bridging the gap between Maori traditional knowledge and mainstream education. Piloted education program to 150+ students across New Zealand. </a>
+							Consulted the NZ Minstry of Education on the 2017/18 NCEA (National Curriculum) reform, and collaborate with other Indigenous nations through the UNPFII.</p>
+					</div>
+
+				</div> <!-- /timeline-block -->
+
+   			</div> <!-- /timeline-wrap -->   			
+
+   		</div> <!-- /col-twelve -->
+   		
+   	</div> <!-- /resume-timeline -->
+   	
+   	<div class="row resume-timeline">
+
+   		<div class="col-twelve resume-header">
+
+   			<h2>Education</h2>
+
+   		</div> <!-- /resume-header -->
+
+   		<div class="col-twelve">
+
+   			<div class="timeline-wrap">
+
+   				<div class="timeline-block">
+
+	   				<div class="timeline-ico">
+	   					<i class="fa fa-graduation-cap"></i>
+	   				</div>
+
+	   				<div class="timeline-header">
+	   					<h3>Master of Science in Management, Global Affairs</h3>
+	   					<p>2022 - 2023</p>
+	   				</div>
+
+	   				<div class="timeline-content">
+	   					<h4>Schwarzman College, Tsinghua University</h4>
+	   					<p>Schwarzman College is designed to meet the challenges of the 21st century and beyond. I was selected as a <a href="https://www.schwarzmanscholars.org/">Schwarzman Scholar</a> for this highly selective program to live in Beijing for a year of study and cultural immersion — attending lectures, traveling around the region, and developing a better understanding of China. .</p>
+	   				</div>
+
+	   			</div> <!-- /timeline-block -->
+
+	   			<div class="timeline-block">
+
+	   				<div class="timeline-ico">
+	   					<i class="fa fa-graduation-cap"></i>
+	   				</div>
+
+	   				<div class="timeline-header">
+	   					<h3>Bachelor of Art, International Politics & Economics</h3>
+	   					<p>2014 - 2018</p>
+	   				</div>
+
+	   				<div class="timeline-content">
+	   					<h4>Middlebury College</h4>
+	   					<p>Fully-funded scholarship to pursue a four-year bachelors of arts at the elite liberal arts college, <a href="https://www.middlebury.edu/">Middlebury College</a> in Vermont, USA. Study-away programs included Oxford University and Semester at Sea (11 country immersion aboard an academic cruise).</p>
+	   				</div>
+
+	   			</div> <!-- /timeline-block -->
+
+	   			<div class="timeline-block">
+
+	   				<div class="timeline-ico">
+	   					<i class="fa fa-graduation-cap"></i>
+	   				</div>
+
+	   				<div class="timeline-header">
+	   					<h3>MPA/MBA Study-Away Program</h3>
+	   					<p>2018</p>
+	   				</div>
+
+	   				<div class="timeline-content">
+	   					<h4>Middlebury Institute for International Studies</h4>
+	   					<p>Engaged in multiple MPA and MBA graduate courses as an undergraduate study-away student. Received Certificate in Conflict Resolution.</p>
+	   				</div>
+
+	   			</div> <!-- /timeline-block -->
+
+				   <div class="timeline-block">
+
+					<div class="timeline-ico">
+						<i class="fa fa-graduation-cap"></i>
+					</div>
+
+					<div class="timeline-header">
+						<h3>Public Policy & International Affairs Fellowship</h3>
+						<p>2017</p>
+					</div>
+
+					<div class="timeline-content">
+						<h4>Goldman School of Public Policy,
+							UC Berkeley</h4>
+						<p><a href="https://ppiaprogram.org/">PPIA</a> is an intensive seven-week summer program that focuses on preparing students for graduate programs in public and international affairs and careers as policy professionals, public administrators and other leadership roles in public service.</p>
+					</div>
+
+				</div> <!-- /timeline-block -->
+
+   			</div> <!-- /timeline-wrap -->   			
+
+   		</div> <!-- /col-twelve -->
+   		
+   	</div> <!-- /resume-timeline -->
+		
+	</section> <!-- /features -->
